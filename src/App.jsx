898f0ac221fb2d0cdae43cad1433ae702634f2c7
Abhishek Roy <abhishek8400roy@gmail.com>
2025-08-31 10:15:06 +0530

    halfway list product done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import Orders from './pages/Orders'
 import Login from './components/login'
  import { ToastContainer} from 'react-toastify';
 
-export  const backendUrl= import.meta.env.VITE_BACKEND_URL;
+export  const backendUrl= import.meta.env.VITE_BACKEND_URL
+
+export const currency = '₹'
 
 
 const App = () => {
